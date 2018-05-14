75c073226a1d9cdde12207783bfe088df00bdefd
behrami <behram1993@gmail.com>
2018-05-14 14:07:18 -0400

    done

--- js/main.js
+++ js/main.js
@@ -4,17 +4,13 @@ var textStatus = document.querySelector('h1');
 
 function changeStatus(){
 
-  // backgroundColor.className = '.dark';
-  // textStatus.innerText='It is dark in here';
-  // button.className = 'switch off';
-
-  if button.classList.contains('on'){
-    backgroundColor.className = '.dark';
+  if (button.classList.contains('on')){
+    backgroundColor.className = 'dark';
     textStatus.innerText='It is dark in here';
     button.className = 'switch off';
   }
-  else if button.classList.contains('off'){
-    backgroundColor.className = '.light';
+  else if (button.classList.contains('off')){
+    backgroundColor.className = 'light';
     textStatus.innerText='It is bright in here';
     button.className = 'switch on';
   }
