044cc3e589bfc2f891cba11d07129bc860609bf1
behrami <behram1993@gmail.com>
2018-05-14 13:37:47 -0400

    work in progress

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+var button = document.querySelector('button');
+var backgroundColor = document.querySelector('body');
+var textStatus = document.querySelector('h1');
 
+function changeStatus(){
 
+  // backgroundColor.className = '.dark';
+  // textStatus.innerText='It is dark in here';
+  // button.className = 'switch off';
+
+  if button.classList.contains('on'){
+    backgroundColor.className = '.dark';
+    textStatus.innerText='It is dark in here';
+    button.className = 'switch off';
+  }
+  else if button.classList.contains('off'){
+    backgroundColor.className = '.light';
+    textStatus.innerText='It is bright in here';
+    button.className = 'switch on';
+  }
+
+}
+
+button.addEventListener('click', changeStatus);
